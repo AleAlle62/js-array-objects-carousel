5ce07da83d67ee5afc64085c0587d3f366b5b7e0
AleAlle62 <basketallegrini@gmail.com>
2022-11-08 20:17:02 +0100

    riscrivere i bottoni + bonus?

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+//array di oggetti
 const arrImages = [
 	{
 		image: 'img/01.webp',
-		title: "Marvel's Spiderman Miles Morale",
+		title: "Marvel's Spiderman Miles Morales",
 		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
 	},
 	{
@@ -26,81 +27,13 @@ const arrImages = [
 	},
 ];
 
-const eleSlider = document.querySelector('.slider');
+// constani di collegamento html 
+const eleImmagineCentrale = document.querySelector('.immmagine-centrale');
+//bottoni
 const eleBtnLeft = document.querySelector('.btn-left');
 const eleBtnRight = document.querySelector('.btn-right');
-const eleSliderThumbs = document.querySelector('.thumbs')
+// parte delle immagini a sinstra 
+const eleImmaginiPiccole = document.querySelector('.immagini-piccole')
 
 
-for (let i = 0; i < arrImages.length; i++) {
-	const eleImg = document.createElement('img');
-    //stampa immagine centrale
-    eleImg.src = arrImages[i].image
-    //stampa titolo e sottotilo dell'immagine primaria
-    eleSliderThumbs.innerHTML += (`<h2 class=titolo>${arrImages[i].title}</h2> <p class=sottotitolo> ${arrImages[i].text}</p>
-    `)    
-
-
-
-	eleImg.classList.add('slider-img');
-	if (i === 0) {
-		eleImg.classList.add('active');
-	}
-	eleSlider.append(eleImg);
-
-	//thumbs
-
-	// const eleThumb = document.createElement('img');
-	// eleThumb.src = arrImages[i];
-	// eleThumb.classList.add('thumb-img');
-	// if (i === 0) {
-	// 	eleThumb.classList.add('active');
-	// }
-	// eleSliderThumbs.append(eleThumb);
-}
-
-
-const listEleImg = document.querySelectorAll('.slider-img');
-// const listThumbs = document.querySelector('.thumb-img') 
-let activeIndex = 0;
-
-
-// BOTTONE SOTTO
-eleBtnRight.addEventListener('click', function () {
-	listEleImg[activeIndex].classList.remove('active');
-	// listThumbs[activeIndex].classList.remove('active');
-
-    // ciclo infinto
-    if (activeIndex === arrImages.length - 1){
-        activeIndex = 0
-    } else {
-        activeIndex++
-    }
-
-	listEleImg[activeIndex].classList.add('active');
-	// listThumbs[activeIndex].classList.add('active');
-
-	eleBtnLeft.classList.remove('hidden');
-	if (activeIndex === listEleImg.length + 1) {
-		eleBtnRight.classList.add('hidden');
-	}
-});
-
-// BOTTONE SOPRA 
-eleBtnLeft.addEventListener('click', function () {
-	listEleImg[activeIndex].classList.remove('active');
-
-    // ciclo infinto
-    if (activeIndex === arrImages.length - 1){
-        activeIndex = 0
-    } else {
-        activeIndex++
-    }
-	listEleImg[activeIndex].classList.add('active');
-
-	eleBtnRight.classList.remove('hidden');
-	if (activeIndex === listEleImg.length + 1) {
-		eleBtnLeft.classList.add('hidden');
-		// listThumbs[activeIndex].classList.add('active');
-	}
-});
+// RIFARE I DUE BOTTONI guardando il mio vecchio, riscrivere tutto
