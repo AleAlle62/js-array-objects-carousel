34a7cd02e2db26af0488919cdf93261f9f5a910d
AleAlle62 <basketallegrini@gmail.com>
2022-11-09 11:34:51 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,95 +1,134 @@
 //array di oggetti
+
 const arrImages = [
 	{
-		image: 'img/01.webp',
+		image: '01.webp',
 		title: "Marvel's Spiderman Miles Morales",
 		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
 	},
 	{
-		image: 'img/02.webp',
+		image: '02.webp',
 		title: 'Ratchet & Clank: Rift Apart',
 		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
 	},
 	{
-		image: 'img/03.webp',
+		image: '03.webp',
 		title: 'Fortnite',
 		text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
 	},
 	{
-		image: 'img/04.webp',
+		image: '04.webp',
 		title: 'Stray',
 		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
 	},
 	{
-		image: 'img/05.webp',
+		image: '05.webp',
 		title: "Marvel's Avengers",
 		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
 	},
-];
+]
 
-// constani di collegamento html 
-const eleImmagineCentrale = document.querySelector('.immmagine-centrale');
-//bottoni
-const eleBtnLeft = document.querySelector('.btn-left');
-const eleBtnRight = document.querySelector('.btn-right');
-// parte delle immagini a sinstra 
-const eleImmaginiPiccole = document.querySelector('.immagini-piccole')
+//creazione delle varibili collegate 
+const eleleftSlider = document.querySelector(".left-slider") //parte sinistra
+
+const elerightSlider = document.querySelector(".right-slider") //parte destra
+
+const eleTitle = document.querySelector(".title") //titolo
+
+const eleText = document.querySelector(".text") //testo
+
+eleTitle.innerHTML = arrImages[0].title 
+
+eleText.innerHTML = arrImages[0].text
 
 
-// RIFARE I DUE BOTTONI guardando il mio vecchio, riscrivere tutto
-
-for (let i = 0; i < arrImages; i++) {
-	//dichiarazione nuove variabili 
-	let eleImg = document.createElement('img')
-	eleImg.src = arrImages[i].image
-	eleImg.classList.add('immagine-centrale-img')
-	//ciclo che fa cambiare le immagini
-	if (i === 0){
-		eleImg.classList.add('active')
-	}
-	eleImmagineCentrale.append('eleImg')
+// slider di sinistra
+for (i = 0; i < arrImages.length; i++) {
+	//creazione variabili img
+    let eleImgGrande = document.createElement("img")
+    eleImgGrande.src = `img/${arrImages[i].image}`
+    eleImgGrande.classList.add('left-slider-img')
+	//condizioni del cambio immagini
+    if(i === 0){
+        eleImgGrande.classList.add('active')
+    }
+    eleleftSlider.append(eleImgGrande)
 }
 
-//altre variabili 
-let listEleImg = document.querySelectorAll('centrale-img')
-let activeIndex = 0
 
-//bottone di sinistra
-eleBtnLeft.addEventListener('click', function (){
-	//far scomparire
-	listEleImg[activeIndex].classList.remove('active')
-	//ciclo infinito
-	if (activeIndex === arrImages.length - 1){
-		activeIndex = 0
-	} else {
-		activeIndex++
-	}
-
-	listEleImg[activeIndex].classList.add('active')
-	eleBtnRight.classList.remove('hidden')
-
-	if (activeIndex === listEleImg.length + 1){
-		eleBtnLeft.classList.add('hidden')
-	}
-})
+// slider di destra delle immagini piccole
+for (i = 0; i < arrImages.length; i++) {
+	//crazioni variabili piccole
+    let eleImgGrande = document.createElement("img")
+    eleImgGrande.src = `img/${arrImages[i].image}`;
+	eleImgGrande.classList.add('right-slider-img')
+	//condizioni per le immagini piccole
+    if(i === 0){
+        eleImgGrande.classList.add('selected')
+    }
+    elerightSlider.append(eleImgGrande)
+}
 
 
-// bottone destra 
 
-eleBtnRight.addEventListener('click', function (){
-	listEleImg[activeIndex].classList.remove('active')
+//variabili per i bottoni
+let immagineAttiva = 0;
 
-	//ciclo infinito
-	if (activeIndex === arrImages.length - 1){
-		activeIndex = 0
-	} else {
-		activeIndex++
-	}
+let eleupButton = document.getElementById("up-button");
 
-	listEleImg[activeIndex].classList.add('active')
-	eleBtnLeft.classList.remove('hidden')
+let eledownButton = document.getElementById("down-button");
 
-	if (activeIndex === listEleImg.length + 1) {
-		eleBtnRight.classList.add('hidden')
-	}
-})
+let elelistLeftImg = document.querySelectorAll('.left-slider-img');
+
+let elelistRightImg = document.querySelectorAll('.right-slider-img');
+
+
+
+
+// funzione 
+function disattiva() {
+    elelistLeftImg[immagineAttiva].classList.remove('active');
+    elelistRightImg[immagineAttiva].classList.remove('selected');
+
+    if(immagineAttiva === 0){
+        immagineAttiva = arrImages.length;
+    }
+
+    immagineAttiva--;
+
+    elelistLeftImg[immagineAttiva].classList.add('active');
+    elelistRightImg[immagineAttiva].classList.add('selected');
+	eleTitle.innerHTML = arrImages[immagineAttiva].title;
+    eleText.innerHTML = arrImages[immagineAttiva].text;
+}
+
+
+//funzione
+function attiva() {
+    elelistLeftImg[immagineAttiva].classList.remove('active');
+    elelistRightImg[immagineAttiva].classList.remove('selected');
+    
+    immagineAttiva++;
+
+    if(immagineAttiva === (arrImages.length)){
+        immagineAttiva = 0;
+    }
+
+    elelistLeftImg[immagineAttiva].classList.add('active');
+    elelistRightImg[immagineAttiva].classList.add('selected');
+    eleTitle.innerHTML = arrImages[immagineAttiva].title;
+	eleText.innerHTML = arrImages[immagineAttiva].text;
+}
+
+
+
+// richiamo delle funzioni per i bottoni 
+eledownButton.addEventListener('click', function(){
+    attiva();
+});
+
+//richiamo delle funzioni per i bottoni
+eleupButton.addEventListener('click', function(){
+    disattiva();
+});
+
